d411f90db87e1e85331c334871f3e535f99d4519
gnivler <gnivil@gmail.com>
2018-06-16 17:12:36 -0600

    initial commit, probably done.  added assets inline, update readme, didn't change version number

--- AdjustedMechSalvage/AdjustedMechSalvage/HolderClasses.cs
+++ AdjustedMechSalvage/AdjustedMechSalvage/HolderClasses.cs
@@ -3,6 +3,8 @@
         public bool ownMechsForFree = false;
         public float ejectRecoveryBonus = 0f;
         public float incapacitatedRecoveryBonus = 0f;
+        public bool centerTorsoHasCustomSalvageValue = false;
+        public float centerTorsoCustomSalvageValue = 0;
     }
 
     public class Fields {
