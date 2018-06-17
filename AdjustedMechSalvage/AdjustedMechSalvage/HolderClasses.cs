7f4a3d44ce4d64bc195931cde0432bfb9d1701f9
Morphyum <zion@c-schwabe.de>
2018-06-17 16:53:10 +0200

    *changed ctsalvage to int

--- AdjustedMechSalvage/AdjustedMechSalvage/HolderClasses.cs
+++ AdjustedMechSalvage/AdjustedMechSalvage/HolderClasses.cs
@@ -3,7 +3,7 @@
         public bool ownMechsForFree = false;
         public float ejectRecoveryBonus = 0.75f;
         public float incapacitatedRecoveryBonus = 0.5f;
-        public float centerTorsoSalvageValue = 1;
+        public int centerTorsoSalvageValue = 1;
     }
 
     public class Fields {
